0f3601006b41c03ca1b257085352b4d0d29c4e29
rmwillow <rmwillowbis2bis@gmail.com>
2021-05-26 11:10:30 -0500

    time color function

--- assets/script.js
+++ assets/script.js
@@ -6,16 +6,47 @@ $(document).ready(function() {
     $(".todaysDate").append(today);
 
 
+    // hh is 24 hours time
+    //parse integer from moment string
+    var currentTime = parseInt(moment().format('HH'));
+    console.log(currentTime)
 
 
-    var $text9AM = $("#text9AM");
-    var $text10AM = $("#text10AM");
-    var $text11AM = $("#text11AM");
-    var $text12AM = $("#text12AM");
-    var $text1PM = $("#text1PM");
-    var $text2PM = $("#text2PM");
-    var $text3PM = $("#text3PM");
-    var $text4PM = $("#text4PM");
-    var $text5PM = $("#text5PM");
+    //set variable to equal id name from html
+    var timeSlot6 = $("#time6AM");
+    var timeSlot7 = $("#time7AM");
+    var timeSlot8 = $("#time8AM");
+    var timeSlot9 = $("#time9AM");
+    var timeSlot10 = $("#time10AM");
+    var timeSlot11 = $("#time11AM");
+    var timeSlot12 = $("#time12AM");
+    var timeSlot1 = $("#time1PM");
+    var timeSlot2 = $("#time2PM");
+    var timeSlot3 = $("#time3PM");
+    var timeSlot4 = $("#time4PM");
+    var timeSlot5 = $("#time5PM");
 
-});
+
+    // selects all text area sections 
+    //.each call back iterates over each text area section
+    $("textarea").each(function() {
+        //converts string into integer
+        //returns attr value of name
+        var name = parseInt($(this).attr("name"));
+        //if less than the current time make gray for not time yet
+        if (name < currentTime) {
+            $(this).addClass("bg-gray");
+        }
+
+        //if time is greater than the current time make green meaning you have more time
+        if (name > currentTime) {
+            $(this).addClass("bg-green")
+        }
+
+        // if time is equal to the current time make red because it is now urgent
+        if (name === currentTime) {
+            $(this).addClass("bg-red")
+        }
+
+    })
+})
