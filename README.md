fce4b58d98b4819bcf664ac6e8f79bdb2901d680
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-05-27 10:40:52 -0500

    Dev (#6)
    * skeleton
    
    * day display
    
    * time color function
    
    * saved items to local storage
    
    * show tasks after page reload
    
    * touch ups

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# Work Day Scheduler Starter Code
+# Simply Cal
+
+## A Work Day Scheduler
+
+> **Purpose**: A website that allows you to save daily tasks for work or personal use.
+
+<br />
+<br />
+
+![Homepage](https://user-images.githubusercontent.com/52016382/119854495-ba21fb00-bed6-11eb-84f9-9bee71605467.png)
+
+
+
+<br />
+<br />
+
+## Displays the current day
+
+![Displays current day](https://user-images.githubusercontent.com/52016382/119854790-fce3d300-bed6-11eb-93e0-3cfe9d15f2f3.png)
+______________________________________________________________________
+## The web site has  3 color coded sections
+
+
+
+> - Gray
+> - Red
+> - Green
+ 
+
+<br />
+<br />
+
+![Colored Sections](https://user-images.githubusercontent.com/52016382/119854568-c7d78080-bed6-11eb-911d-471e64ce1d57.png)
+
+<br/>
+
+
+_________________________________________________________________________
+<br />
+
+## Lock buttons to save your tasks to local storage !
+
+<br />
+
+![Local Storage](https://user-images.githubusercontent.com/52016382/119854718-ed648a00-bed6-11eb-966a-d7484124a1fb.png)
+
+
+_________________________________________________________________________
+<br />
+
+<br />
+
+> For more information visit the site clicking here: [Rachel McGraths Portfolio](https://rmwillow.github.io/portfolio/)
+
+<br/>
+
+## Built With
+* HTML
+* CSS
+* jquery
+* JavaScript
+
+## Contribution
+Made by Rachel McGrath
+
+### ©️2021 
---
+++ assets/script.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+    //Setting up the date and appending to the HTML page
+    var today = moment().format('MMMM Do YYYY');
+    $(".todaysDate").append(today);
+    // console.log(today)
+
+    // HH is 24 hours time
+    var currentTime = parseInt(moment().format('HH'));
+    // console.log(currentTime)
+
+    //set variable to equal id name from html
+    var timeSlot6 = $("#time6AM");
+    var timeSlot7 = $("#time7AM");
+    var timeSlot8 = $("#time8AM");
+    var timeSlot9 = $("#time9AM");
+    var timeSlot10 = $("#time10AM");
+    var timeSlot11 = $("#time11AM");
+    var timeSlot12 = $("#time12AM");
+    var timeSlot1 = $("#time1PM");
+    var timeSlot2 = $("#time2PM");
+    var timeSlot3 = $("#time3PM");
+    var timeSlot4 = $("#time4PM");
+    var timeSlot5 = $("#time5PM");
+
+    // selects all time area sections 
+    //.each is a call back & iterates over each textarea section
+    $("textarea").each(function() {
+        //converts string into integer
+        //returns attr value of name
+        var name = parseInt($(this).attr("name"));
+        //if less than the current time make gray for not time yet
+        if (name < currentTime) {
+            //add color class
+            $(this).addClass("bg-gray");
+        }
+
+        //if time is greater than the current time make green meaning you have more time
+        if (name > currentTime) {
+            //add color class
+            $(this).addClass("bg-green")
+        }
+
+        // if time is equal to the current time make red because it is now urgent
+        if (name === currentTime) {
+            //add color class
+            $(this).addClass("bg-red")
+        }
+    })
+
+    // setting each items in the local storage with new labels to show in local storage output
+    $("button").on("click", function() {
+        localStorage.setItem("6 AM Task", (timeSlot6.val()))
+        localStorage.setItem("7 AM Task", (timeSlot7.val()))
+        localStorage.setItem("8 AM Task", (timeSlot8.val()))
+        localStorage.setItem("9 AM Task", (timeSlot9.val()))
+        localStorage.setItem("10 AM Task", (timeSlot10.val()))
+        localStorage.setItem("11 AM Task", (timeSlot11.val()))
+        localStorage.setItem("12 AM Task", (timeSlot12.val()))
+        localStorage.setItem("1 PM Task", (timeSlot1.val()))
+        localStorage.setItem("2 PM Task", (timeSlot2.val()))
+        localStorage.setItem("3 PM Task", (timeSlot3.val()))
+        localStorage.setItem("4 PM Task", (timeSlot4.val()))
+        localStorage.setItem("5 PM Task", (timeSlot5.val()))
+    })
+
+    //getting the content stored and sending to the screen. When page is refreshed content will stay
+    $("#time6AM").append(localStorage.getItem("6 AM Task"));
+    $("#time7AM").append(localStorage.getItem("7 AM Task"));
+    $("#time8AM").append(localStorage.getItem("8 AM Task"));
+    $("#time9AM").append(localStorage.getItem("9 AM Task"));
+    $("#time10AM").append(localStorage.getItem("10 AM Task"));
+    $("#time11AM").append(localStorage.getItem("11 AM Task"));
+    $("#time12AM").append(localStorage.getItem("12 AM Task"));
+    $("#time1PM").append(localStorage.getItem("1 PM Task"));
+    $("#time2PM").append(localStorage.getItem("2 PM Task"));
+    $("#time3PM").append(localStorage.getItem("3 PM Task"));
+    $("#time4PM").append(localStorage.getItem("4 PM Task"));
+    $("#time5PM").append(localStorage.getItem("5 PM Task"));
+})
